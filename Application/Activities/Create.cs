b7e871a615e6708bfb027c9ee599d2e22cea446d
Artrit <artrit.dili@hotmail.com>
2024-01-09 13:02:27 +0100

    added crud actions for activities

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,28 @@
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+            public Handler(DataContext context)
+            {
+                _dataContext = context;
+            }
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                _dataContext.Activities.Add(request.Activity);
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
